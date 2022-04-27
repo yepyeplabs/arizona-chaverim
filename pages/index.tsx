8a17949ed0bd9b7ad05922924ef0b68a8887fa06
Ira Sanchez <ira.d.sanchez@gmail.com>
2022-04-26 20:19:30 -0700

    Removes nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,71 +10,7 @@ const Home = () => {
     <Layout>
 
       <section className="relative bg-gray-900 pb-20">
-        <nav className="flex justify-between items-center py-8 px-4 xl:px-10">
-          <a className="text-lg font-semibold" href="#">
-            <img
-              className="h-7"
-              src="zeus-assets/logo/logo-zeus-white.svg"
-              alt=""
-              width="auto"
-            />
-          </a>
-          <div className="lg:hidden">
-            <button className="navbar-burger flex items-center p-3 hover:bg-blue-300 rounded">
-              <svg
-                className="text-white block h-4 w-4"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-              >
-                <title>Mobile menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-          <ul className="hidden lg:ml-auto lg:mr-12 lg:flex lg:items-center lg:space-x-12">
-            <li>
-              <a
-                className="text-white hover:text-blue-100 text-sm font-semibold"
-                href="#"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-white hover:text-blue-100 text-sm font-semibold"
-                href="#"
-              >
-                Company
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-white hover:text-blue-100 text-sm font-semibold"
-                href="#"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-white hover:text-blue-100 text-sm font-semibold"
-                href="#"
-              >
-                Testimonials
-              </a>
-            </li>
-          </ul>
-          <div className="hidden lg:block">
-            <a
-              className="inline-block py-3 px-8 text-sm leading-normal font-medium rounded bg-blue-500 bg-opacity-25 hover:bg-opacity-50 text-blue-500 transition duration-200"
-              href="#"
-            >
-              Contact Us
-            </a>
-          </div>
-        </nav>
+        
         <img
           className="hidden lg:block lg:absolute top-0 left-0 mt-32"
           src="zeus-assets/icons/dots/yellow-dot-left-bars.svg"
